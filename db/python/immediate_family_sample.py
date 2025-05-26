5a0634b458df1ffd125b9971e415203ebea5d69c
scMarth <vlantaca@gmail.com>
2025-05-25 23:10:22 -0700

    update info dat

---
+++ db/python/immediate_family_sample.py
@@ -0,0 +1,10 @@
+# example of the contents of immediate_family.py, create separately
+
+mother = ('John Doe Mom', 'Doe', 'Before', '1920-08-08')
+
+father = ('John Doe Dad', 'Doe', '', '1920-08-08')
+
+children = [
+    ('John', 'Doe', '', '1920-08-08'),
+    ('Timmy', 'Doe', '', '1920-08-08')
+]
